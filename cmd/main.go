7c19315985946c87e2e5b4291c19a8afa2b85de7
Poletansky Viktor <poletansky@gmail.com>
2022-12-10 04:09:06 +0300

    xdd

--- cmd/main.go
+++ cmd/main.go
@@ -75,6 +75,7 @@ var (
 					client = balaboba.ClientEng
 				}
 
+				// TODO: fix 403
 				r, err := client.Generate(ctx.Context, text, *ctx.Generic("style").(*balaboba.Style))
 				if err != nil {
 					return err
